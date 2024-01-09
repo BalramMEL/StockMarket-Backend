15e41e66711fa2379a0620f540ac274292d9e0a7
Balram Uddhav Melkunde <102988461+BalramMEL@users.noreply.github.com>
2024-01-10 01:28:40 +0530

    setup & models

---
+++ Models/Comment.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace web_api.Models
+{
+    public class Comment
+    {
+        public int Id{ get; set; }
+
+        public string Title { get; set; } = string.Empty;
+        public string Content { get; set; } = string.Empty;
+        public DateTime CreatedOn { get; set; } = DateTime.Now;
+        public int? StockId {get; set;}
+        public Stock? Stock {get; set;}        
+    }
+}
---
+++ Models/Stock.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace web_api.Models
+{
+    public class Stock
+    {
+        public int Id { get; set; }
+        public string Symbol { get; set; } = string.Empty;
+
+        public string CompanyName { get; set; } = string.Empty;
+        [Column(TypeName = "decimal(18, 2)")]
+
+        public decimal Purchase{ get; set; }
+        [Column(TypeName = "decimal(18, 2)")]
+
+        public decimal LastDiv { get; set; }
+
+        public string Industry{ get; set; } = string.Empty;
+
+        public long MarketCap { get; set; }
+
+        public List<Comment> Comments {get; set; } = new List<Comment>();
+    }
+}
